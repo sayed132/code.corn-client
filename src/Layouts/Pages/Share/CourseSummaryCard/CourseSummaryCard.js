65e3d9f8329fd9bd65c47bd3650988adf662f5a5
Abu Sayed <rafsansayed132@gmail.com>
2022-10-25 05:23:58 +0600

    added author information

--- src/Layouts/Pages/Share/CourseSummaryCard/CourseSummaryCard.js
+++ src/Layouts/Pages/Share/CourseSummaryCard/CourseSummaryCard.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaStar , FaEye, FaPencilAlt} from 'react-icons/fa';
 
 const CourseSummaryCard = ({ course }) => {
     console.log(course);
     const { author, details, image_url, others_info, rating, thumbnail_url, title, total_view, _id } = course;
     return (
-
-        <div className="card w-100 bg-base-100 shadow-xl">
-            <figure><img src={thumbnail_url} alt="thumbnail" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {title}
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>{details}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">{total_view}</div>
-                    <div className="badge badge-outline">{author?.name}</div>
+        <Link to={`/course/${_id}`}>
+            <div style={{ height: '550px' }} className="card w-100 bg-base-100 shadow-xl">
+                <figure><img src={thumbnail_url} alt="thumbnail" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {title}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>{details}</p>
+                    <div className="card-actions justify-between items-center">
+                        
+                        <div className="card-actions items-center justify-start">
+                        <img className="badge badge-outline w-100 btn-circle"  src={author?.img} alt="" />
+                        <div className="badge badge-outline"><FaPencilAlt></FaPencilAlt> &nbsp;{author?.name}</div>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline"><FaEye className='text-muted'></FaEye>&nbsp;{total_view}</div>
+                            <div className="badge badge-outline"><FaStar className='text-warning'></FaStar>&nbsp;{rating?.number}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
